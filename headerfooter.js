31823b3012f7b4cac5613bd3e01c359cecf2a528
David Tang <davidtang.int@gmail.com>
2023-12-30 23:26:15 -0700

    optimizing header+footer using js

---
+++ headerfooter.js
@@ -0,0 +1,47 @@
+class CustomHeader extends HTMLElement{
+    connectedCallback() {
+        this.innerHTML = `
+        <nav>
+            <a href="index.html" class="name">DAVID J TANG</a>
+            <ul>
+                <li><a href="index.html#portfolio">Portfolio</a></li>
+                <li><a href="index.html#about">About</a></li>
+                <li><a href="https://www.linkedin.com/in/davidtangint/" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+                <li><a href="mailto: davidtang.int@gmail.com"><i class="fa-regular fa-envelope"></i></a></li>
+            </ul>
+        </nav>
+        `
+    }
+}
+
+class CustomWorkHeader extends HTMLElement{
+    connectedCallback() {
+        this.innerHTML = `
+        <nav>
+            <a href="index.html" class="name">DAVID J TANG</a>
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="index.html#about">About</a></li>
+                <li><a href="https://www.linkedin.com/in/davidtangint/" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+                <li><a href="mailto: davidtang.int@gmail.com"><i class="fa-regular fa-envelope"></i></a></li>
+            </ul>
+        </nav>
+        `
+    }
+}
+
+class CustomFooter extends HTMLElement{
+    connectedCallback() {
+        this.innerHTML = `
+        <div id="copyright">
+            <div class="container">
+                <p>© Designed by David Tang | Last Updated 2023/12/30</p>
+            </div>
+        </div>
+        `
+    }
+}
+
+customElements.define('custom-header', CustomHeader)
+customElements.define('custom-work-header', CustomWorkHeader)
+customElements.define('custom-footer', CustomFooter)
